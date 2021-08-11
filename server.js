aff32338be66bbe68a92559d263e1cbc3a703d78
boxuan-L <boxuanc4d@gmail.com>
2021-08-11 10:25:16 +0200

    init server and index route

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+
+const indexRouter = require("./routes/index");
+
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 3000);
