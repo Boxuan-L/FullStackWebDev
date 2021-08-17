78bbab1f0b2330d0fdaa38c50e5b175f05d6a5d1
boxuan-L <boxuanc4d@gmail.com>
2021-08-17 18:17:49 +0200

    Finish author routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Author = require("../models/author");
+const Book = require("../models/book");
 
 // All authors
 router.get("/", async (req, res) => {
@@ -26,6 +27,9 @@ router.get("/", async (req, res) => {
 });
 
 // New authors
+// the program will search route from top to bottom.
+// so make sure to put the route that has :id after the normal route
+// otherwise it will treat the word as id
 router.get("/new", (req, res) => {
   res.render("authors/new", { author: new Author() });
 });
@@ -37,24 +41,77 @@ router.post("/new", async (req, res) => {
   });
   try {
     const newAuthor = await author.save();
-    res.redirect("/authors");
+    res.redirect(`/authors/${newAuthor.id}`);
   } catch (error) {
     res.render("authors/new", {
       author: author,
       errorMessage: "Error creating Author",
     });
   }
-  // author.save((err, newAuthor) => {
-  //   if (err) {
-  //     res.render("authors/new", {
-  //       author: author,
-  //       errorMessage: "Error creating Author",
-  //     });
-  //   } else {
-  //     //res.redirect("/authors/${newAuthor.id}");
-  //     res.redirect("/authors");
-  //   }
-  // });
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    // remember to use exec() function after limit()
+    const books = await Book.find({ author: author.id }).limit(6).exec();
+    res.render("authors/show", {
+      author: author,
+      booksByAuthor: books,
+    });
+  } catch {
+    res.redirect("/");
+  }
+});
+
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    res.render("authors/edit", { author: author });
+  } catch (error) {
+    res.redirect("/authors");
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  let author;
+  try {
+    author = await Author.findById(req.params.id);
+    author.name = req.body.name;
+    await author.save();
+    res.redirect(`/authors/${author.id}`);
+  } catch (error) {
+    // there are two places that might have error
+    // this is for the findById function
+    if (author == null) {
+      res.redirect("/");
+    } else {
+      // if the author does exist, then this is for the save method
+      res.render("authors/edit", {
+        author: author,
+        errorMessage: "Error Updating Author",
+      });
+    }
+  }
+});
+
+// never use get for deleting. Because search engine will
+// click on every get. This will accidently delete everythihg
+router.delete("/:id", async (req, res) => {
+  let author;
+  try {
+    author = await Author.findById(req.params.id);
+    await author.remove();
+    res.redirect(`/authors`);
+  } catch (error) {
+    // there are two places that might have error
+    // this is for the findById function
+    if (author == null) {
+      res.redirect("/");
+    } else {
+      res.redirect(`/authors/${author.id}`);
+    }
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require("method-override");
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
@@ -16,6 +17,7 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.use(express.urlencoded());
+app.use(methodOverride("_method"));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
